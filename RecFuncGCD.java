eed871384e9cd889640e383b787d13244161dd24
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-10-15 10:55:03 +0530

    Create RecFuncGCD.java

---
+++ RecFuncGCD.java
@@ -0,0 +1,18 @@
+// Java program to find GCD of two numbers 
+class RecFuncGCD
+{ 
+	// Recursive function to return gcd of a and b 
+	static int gcd(int a, int b) 
+	{ 
+	if (b == 0) 
+		return a; 
+	return gcd(b, a % b); 
+	} 
+	
+	// Driver method 
+	public static void main(String[] args) 
+	{ 
+		int a = 98, b = 56; 
+		System.out.println("GCD of " + a +" and " + b + " is " + gcd(a, b)); 
+	} 
+} 
