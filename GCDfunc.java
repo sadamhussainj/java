55ddaca4d6a24f5496da2030b91f0679c23cec1c
SadamHussain J <47327911+sadamhussainj@users.noreply.github.com>
2019-10-15 10:56:41 +0530

    Update GCDfunc.java

--- GCDfunc.java
+++ GCDfunc.java
@@ -1,26 +1,24 @@
-// Java program to find GCD of two numbers 
-class Test 
+class GCDfunc
 { 
-	// Recursive function to return gcd of a and b 
 	static int gcd(int a, int b) 
 	{ 
-		// Everything divides 0 
+		
 		if (a == 0) 
 		return b; 
 		if (b == 0) 
 		return a; 
 	
-		// base case 
+		
 		if (a == b) 
 			return a; 
 	
-		// a is greater 
+		
 		if (a > b) 
 			return gcd(a-b, b); 
 		return gcd(a, b-a); 
 	} 
 	
-	// Driver method 
+	
 	public static void main(String[] args) 
 	{ 
 		int a = 98, b = 56; 
